8160088bdd59d1cdd0162330b9b3f6abf276320e
Vasu Gadde <vasugadde0203@gmail.com>
2024-10-18 23:20:26 +0530

    added static files

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,13 +26,8 @@ SECRET_KEY = 'django-insecure--l&x2q&-6trb+=-=shaf52ifhdv)@q&nxzo$l3x%j15yn9zvoa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
-# Allow all hosts for Heroku
-ALLOWED_HOSTS = ['*']
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 
 # Application definition
@@ -128,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
